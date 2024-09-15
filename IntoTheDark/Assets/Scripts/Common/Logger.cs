6f6dcd39136818deebfb292653802c122a585bfe
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2024-09-15 10:07:16 +0900

    [Fix] Logger 수정

--- IntoTheDark/Assets/Scripts/Common/Logger.cs
+++ IntoTheDark/Assets/Scripts/Common/Logger.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Logger : MonoBehaviour
+public class Logger : Singleton<Logger>
 {
-#if UNITY_EDITOR
     public void Log(string msg)
     {
+#if UNITY_EDITOR
         Debug.Log(msg);
+#endif
     }
 
     public void LogWarning(string msg)
     {
+#if UNITY_EDITOR
         Debug.LogWarning(msg);
-    }
 #endif
+    }
 
     public void LogError(string msg)
     {
